61b7652e91ab2bcb47d8a83e644860eb6fc907e9
snowadv <log.mihail2003@gmail.com>
2022-11-24 20:52:16 +0300

    Dijkstra's algo - class implemented

--- js/validator.js
+++ js/validator.js
@@ -419,3 +419,138 @@ var recursiveFindPath = function(currentLineId, destinationId, passedLines, path
 	}
 }
 
+var getConnectedNodes = function(node) {
+	let ways = node.getAttribute("ways").split(",");
+	for(let i = 0; i < ways.length; i++) {
+		
+	}
+}
+
+var createGraph = function() {
+	let graph = new Graph();
+	for(let i = 0; i < lines.length; i++) {
+		if(!isPortalOrDoor(lines[i])) {
+			let line = lines[i];
+
+		}
+	}
+}
+
+
+class Graph
+{
+    constructor() {
+        this.nodes = [];
+        this.adjacencyList = {};
+    }
+
+    addNode(node) {
+        this.nodes.push(node);
+        this.adjacencyList[node] = [];
+    }
+
+	checkIfNodeExistsById(id) {
+		for(let i = 0; i < this.nodes.length; i++) {
+			if(typeof(node[i].id) != "undefined" && node[i].id == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	getNodeById(id) {
+		for(let i = 0; i < this.nodes.length; i++) {
+			if(typeof(node[i].id) != "undefined" && node[i].id == id) {
+				return node[i];
+			}
+		}
+		return null;
+	}
+
+	checkIfNodeExists(node) {
+		for(let i = 0; i < this.nodes.length; i++) {
+			if(node[i] == node) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+    addEdge(node1, node2, weight) {
+        this.adjacencyList[node1].push({node:node2, weight: weight});
+        this.adjacencyList[node2].push({node:node1, weight: weight});
+    }
+
+    findPathWithDijkstra(startNode, endNode) {
+        let times = {};
+        let backtrace = {};
+        let pq = new PriorityQueue();
+
+        times[startNode] = 0;
+
+        this.nodes.forEach(node => {
+            if (node !== startNode) {
+                times[node] = Infinity
+            }
+        });
+
+        pq.enqueue([startNode, 0]);
+
+        while (!pq.isEmpty()) {
+            let shortestStep = pq.dequeue();
+            let currentNode = shortestStep[0];
+            this.adjacencyList[currentNode].forEach(neighbor => {
+                let time = times[currentNode] + neighbor.weight;
+
+                if (time < times[neighbor.node]) {
+                    times[neighbor.node] = time;
+                    backtrace[neighbor.node] = currentNode;
+                    pq.enqueue([neighbor.node, time]);
+                }
+            });
+        }
+
+        let path = [endNode];
+        let lastStep = endNode;
+        while(lastStep !== startNode) {
+            path.unshift(backtrace[lastStep])
+            lastStep = backtrace[lastStep]
+        }
+        console.log(`Path is ${path} and time is ${times[endNode]}`)
+        return path
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.collection = [];
+    }
+
+    enqueue(element){
+        if (this.isEmpty()){
+            this.collection.push(element);
+        } else {
+            let added = false;
+            for (let i = 1; i <= this.collection.length; i++){
+                if (element[1] < this.collection[i-1][1]){
+                    this.collection.splice(i-1, 0, element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added){
+                this.collection.push(element);
+            }
+        }
+    };
+
+    dequeue() {
+        let value = this.collection.shift();
+        return value;
+    };
+    isEmpty() {
+        return (this.collection.length === 0)
+    };
+}
